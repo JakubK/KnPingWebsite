6534c4c3681bdc72905d5278eed696bdf062cd10
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-20 23:35:02 +0200

    Add footer component and mount it in the App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 import Navbar from './components/navbar';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="app">
       <Navbar/>
-
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css';
+
+const footer = () => {
+  return <footer className="footer">
+    <span className="footer__caption">web powered by ...</span>
+    <span className="footer__caption">designed by kocurrok</span>
+  </footer>
+}
+
+export default footer;
