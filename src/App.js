b547938f114985ddbf587cc74013ae0487f98542
mobaradev <mobaradev@yahoo.com>
2021-10-22 01:08:32 +0200

    Add router from 'react-router-dom'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    useLocation
+} from "react-router-dom";
 import './App.css';
 
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
 
+
+
 function App() {
-  return (
-    <div className="app">
-      <Navbar/>
-      <Footer/>
-    </div>
-  );
+    function GetPath() {
+        const location = useLocation();
+        return location.pathname;
+    }
+
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route path="/">
+                        <Navbar path={GetPath} />
+                        <Footer />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
