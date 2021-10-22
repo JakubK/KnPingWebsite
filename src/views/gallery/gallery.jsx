8debd84aa1d731daa7e389232bc6fa069e6856df
mobaradev <mobaradev@yahoo.com>
2021-10-23 01:17:21 +0200

    Add initial random photo to the gallery

--- src/views/gallery/gallery.jsx
+++ src/views/gallery/gallery.jsx
@@ -1,5 +1,8 @@
 const Gallery = () => {
-  return <div>Gallery</div> 
+  return <div>
+    Gallery
+    <img src="https://pliki.horecatrends.pl/i/01/20/57/012057_r2_940.jpg" />
+  </div>
 }
 
 export default Gallery;
