6c7beb1b834ed9066620e717caafa1cb0944cc21
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-28 20:20:25 +0200

    Add base contact page structure without styling

--- src/views/contact/contact.jsx
+++ src/views/contact/contact.jsx
@@ -1,5 +1,30 @@
+import './contact.css'
+
+import map from '../../assets/map.png'
+
 const Contact = () => {
-  return <div>Contact</div> 
+  return <div className="contact">
+    <div>
+      <h1>Napisz do nas</h1>
+      <div>
+        <h3>aby dołączyć do Koła,</h3>
+        <h3>aby nawiązać współpracę,</h3>
+        <h3>aby nas lepiej poznać.</h3>
+      </div>
+
+      <div>
+        <h3>mail: ping@ping.com</h3>
+        <h3>FB: Ping PG</h3>
+        <h3>tel: 123 345 789</h3>
+      </div>
+    </div>
+    <hr className="contact__separator"></hr>
+
+    <div>
+      <h3>Tu jesteśmy!</h3>
+      <img alt="map" src={map}/>
+    </div>
+  </div> 
 }
 
 export default Contact;
