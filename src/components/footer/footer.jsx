c08b8f97bfb06a20978312dd94af062b966bcae2
mobaradev <mobaradev@yahoo.com>
2021-11-19 20:29:14 +0100

    Hide footer on main page /

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,15 @@
 import './footer.css';
 
-const Footer = () => {
-  return <footer className="footer">
-    <span className="footer__caption">web powered by ...</span>
-    <span className="footer__caption">designed by kocurrok</span>
-  </footer>
+const Footer = props => {
+  const path = props.path();
+  const isVisible = (path !== "/");
+
+  return (isVisible ?
+      <footer className="footer">
+        <span className="footer__caption">web powered by ...</span>
+        <span className="footer__caption">designed by kocurrok</span>
+      </footer>
+      : "")
 }
 
 export default Footer;
